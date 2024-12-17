74558ccdcbe9f3f7e1003dcb8a7aa68709832784
xaridar <xaridara@gmail.com>
2024-12-17 16:34:35 -0500

    not much tbh

--- src/components/logo_carousel.tsx
+++ src/components/logo_carousel.tsx
@@ -31,8 +31,7 @@ export const LogoCarousel = (props: LogoCarouselProps) => {
 	const dupe = [...props.products, ...props.products];
 
 	return (
-		<div
-			className='m-auto relative overflow-hidden h-10 py-12 w-full max-w-6xl box-content'>
+		<div className='m-auto relative overflow-hidden h-10 py-12 w-full max-w-6xl box-content'>
 			<div
 				id='animatedCarousel'
 				ref={carouselRef}
@@ -42,31 +41,35 @@ export const LogoCarousel = (props: LogoCarouselProps) => {
 					animationPlayState: paused ? 'paused' : 'running',
 				})}>
 				{dupe.map((p, i) => (
-						<a
-							target='_blank'
-							href={p.link}
-							className='transition-transform h-full'
-							data-tooltip-id='my-tooltip'
-							data-tooltip-content={p.name}
-							key={`${p.name}-${i}`}>
-							<Image
-								onMouseEnter={() => setPaused(true)}
-								onMouseLeave={() => setPaused(false)}
-								className='aspect-initial h-full w-auto lang-icon'
-								src={p.logo}
-								alt={p.name + ' logo'}></Image>
-						</a>
-					))}
+					<a
+						target='_blank'
+						href={p.link}
+						className='transition-transform h-full'
+						data-tooltip-id='my-tooltip'
+						data-tooltip-content={p.name}
+						key={`${p.name}-${i}`}>
+						<Image
+							onMouseEnter={() => setPaused(true)}
+							onMouseLeave={() => setPaused(false)}
+							className='aspect-initial h-full w-auto lang-icon'
+							src={p.logo}
+							alt={p.name + ' logo'}></Image>
+					</a>
+				))}
 			</div>
 			<div
 				className='absolute inset-0 h-full z-10 pointer-events-none hidden sm:block'
 				css={css({
-					background: `linear-gradient(90deg, rgb(var(--background-rgb)) 0%, rgb(var(--background-rgb)) 25%, rgba(var(--background-rgb), ${+paused * 0.75}) 40%, rgba(var(--background-rgb), ${+paused * 0.75}) 60%, rgb(var(--background-rgb)) 75%, rgb(var(--background-rgb)) 100%)`,
+					background: `linear-gradient(90deg, rgb(var(--background-rgb)) 0%, rgb(var(--background-rgb)) 25%, rgba(var(--background-rgb), 0) 40%, rgba(var(--background-rgb), 0) 60%, rgb(var(--background-rgb)) 75%, rgb(var(--background-rgb)) 100%)`,
 				})}></div>
 			<div
 				className='absolute inset-0 h-full z-10 pointer-events-none block sm:hidden'
 				css={css({
-					background: `linear-gradient(90deg, rgb(var(--background-rgb)) 0%, rgb(var(--background-rgb)) ${100/12}%, rgba(var(--background-rgb), ${+paused * 0.75}) 40%, rgba(var(--background-rgb), ${+paused * 0.75}) 60%, rgb(var(--background-rgb)) ${1100/12}%, rgb(var(--background-rgb)) 100%)`,
+					background: `linear-gradient(90deg, rgb(var(--background-rgb)) 0%, rgb(var(--background-rgb)) ${
+						100 / 12
+					}%, rgba(var(--background-rgb), 0) 40%, rgba(var(--background-rgb), 0) 60%, rgb(var(--background-rgb)) ${
+						1100 / 12
+					}%, rgb(var(--background-rgb)) 100%)`,
 				})}></div>
 			<div className='absolute inset-0 w-1/12 sm:w-1/4'></div>
 			<div className='absolute inset-0 w-1/12 sm:w-1/4 left-auto'></div>
