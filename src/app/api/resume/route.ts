91e1b3991e56600662f03f75815a405c35efe93d
xaridar <xaridara@gmail.com>
2025-03-14 12:46:43 -0400

    remove number from resume

--- src/app/api/resume/route.ts
+++ src/app/api/resume/route.ts
@@ -13,31 +13,10 @@ export const GET = async req => {
 			});
 		const file = await resume.blob();
 		const ab = await file.arrayBuffer();
-		const pdfDoc = await PDFDocument.load(ab);
-		pdfDoc.registerFontkit(fontkit);
-		const page = pdfDoc.getPages()[0];
 
-		const { width, height } = page.getSize();
-
-		page.drawRectangle({ x: width - 220, y: height - 65, width: 100, height: 15, color: rgb(1, 1, 1) });
-		const fontBytes = await readFile(path.join(process.cwd(), 'Gelasio-VariableFont_wght.ttf'));
-		const customFont = await pdfDoc.embedFont(new Uint8Array(fontBytes));
-
-		page.drawText('(***)-***-****', {
-			x: width - 215,
-			y: height - 62,
-			size: 10,
-			color: rgb(0, 0, 0),
-			font: customFont,
-		});
-
-		const newBytes = await pdfDoc.save();
-		const buf = Buffer.from(newBytes.buffer);
-		const len = buf.length;
-
-		return new Response(buf, {
+		return new Response(ab, {
 			status: 200,
-			headers: { 'Content-Type': 'application/pdf', 'Content-Length': `${len}` },
+			headers: { 'Content-Type': 'application/pdf', 'Content-Length': `${ab.byteLength}` },
 		});
 	} catch (error) {
 		console.error(error);
